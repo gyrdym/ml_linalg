bfee8ce45441bcb46735d26963ca7c3ea4b53030
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-07-09 10:36:31 +0300

    benchmarks corrected

--- example/benchmark/vector_addition.dart
+++ example/benchmark/vector_addition.dart
@@ -1,26 +1,12 @@
+// Performance test of vector (10 000 000 elements in vector) addition operation
+
 import 'package:dart_vector/vector.dart';
 import 'package:benchmark_harness/benchmark_harness.dart';
 
-const int AMOUNT_OF_ELEMENTS = 1000000;
+const int AMOUNT_OF_ELEMENTS = 10000000;
 
 Float32x4Vector vector1;
 Float32x4Vector vector2;
-
-class VectorInitializationBenchmark extends BenchmarkBase {
-  const VectorInitializationBenchmark() : super('Vector initialization, $AMOUNT_OF_ELEMENTS elements');
-
-  static void main() {
-    new VectorInitializationBenchmark().report();
-  }
-
-  void run() {
-    vector1 = new Float32x4Vector.from(new List<double>.filled(AMOUNT_OF_ELEMENTS, 1.0));
-  }
-
-  void tearDown() {
-    vector1 = null;
-  }
-}
 
 class VectorAdditionBenchmark extends BenchmarkBase {
   const VectorAdditionBenchmark() : super('Vectors addition, $AMOUNT_OF_ELEMENTS elements');
@@ -45,6 +31,5 @@ class VectorAdditionBenchmark extends BenchmarkBase {
 }
 
 void main() {
-//  VectorAdditionBenchmark.main();
-  VectorInitializationBenchmark.main();
+  VectorAdditionBenchmark.main();
 }
