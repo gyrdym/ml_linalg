540aa34d235fa0435488499f2075dd9fdeb63c84
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-07-09 12:14:00 +0300

    eamples added

--- example/vector_operations.dart
+++ example/vector_operations.dart
@@ -9,6 +9,12 @@ void main() {
   manhattanNorm();
   mean();
   sum();
+  dot();
+  scalarAddition();
+  scalarSubtraction();
+  scalarMultiplication();
+  scalarDivision();
+  euclideanDistanceBetweenVectors();
 }
 
 void addition() {
@@ -61,4 +67,46 @@ void sum() {
   Float32x4Vector vector1 = new Float32x4Vector.from([2.0, 3.0, 4.0, 5.0, 6.0]);
   double result = vector1.sum();
   print(result); // 2 + 3 + 4 + 5 + 6 = 20.0 (equivalent to Manhattan norm)
+}
+
+void dot() {
+  Float32x4Vector vector1 = new Float32x4Vector.from([1.0, 2.0, 3.0, 4.0, 5.0]);
+  Float32x4Vector vector2 = new Float32x4Vector.from([2.0, 3.0, 4.0, 5.0, 6.0]);
+  double result = vector1.dot(vector2);
+  print(result); // 1.0 * 2.0 + 2.0 * 3.0 + 3.0 * 4.0 + 4.0 * 5.0 + 5.0 * 6.0 = 70.0
+}
+
+void scalarAddition() {
+  Float32x4Vector vector1 = new Float32x4Vector.from([1.0, 2.0, 3.0, 4.0, 5.0]);
+  double scalar = 5.0;
+  Float32x4Vector result = vector1.scalarAdd(scalar);
+  print(result.asList()); // [6.0, 7.0, 8.0, 9.0, 10.0]
+}
+
+void scalarSubtraction() {
+  Float32x4Vector vector1 = new Float32x4Vector.from([1.0, 2.0, 3.0, 4.0, 5.0]);
+  double scalar = 5.0;
+  Float32x4Vector result = vector1.scalarSub(scalar);
+  print(result.asList()); // [-4.0, -3.0, -2.0, -1.0, 0.0]
+}
+
+void scalarMultiplication() {
+  Float32x4Vector vector1 = new Float32x4Vector.from([1.0, 2.0, 3.0, 4.0, 5.0]);
+  double scalar = 5.0;
+  Float32x4Vector result = vector1.scalarMul(scalar);
+  print(result.asList()); // [5.0, 10.0, 15.0, 20.0, 25.0]
+}
+
+void scalarDivision() {
+  Float32x4Vector vector1 = new Float32x4Vector.from([25.0, 50.0, 75.0, 100.0, 125.0]);
+  double scalar = 5.0;
+  Float32x4Vector result = vector1.scalarDiv(scalar);
+  print(result.asList()); // [5.0, 10.0, 15.0, 20.0, 25.0]
+}
+
+void euclideanDistanceBetweenVectors() {
+  Float32x4Vector vector1 = new Float32x4Vector.from([1.0, 2.0, 3.0, 4.0, 5.0]);
+  Float32x4Vector vector2 = new Float32x4Vector.from([2.0, 3.0, 4.0, 5.0, 6.0]);
+  double result = vector1.distanceTo(vector2);
+  print(result); // ~~2.23
 }
