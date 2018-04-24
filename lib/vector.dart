be8729bcb5dcaaa8501dbb4aadaad93c18b55a8f
Ilia Gyrdymov <ilgyrd@gmail.com>
2018-04-25 01:10:07 +0300

    refactored

--- lib/vector.dart
+++ lib/vector.dart
@@ -1,3 +1,3 @@
 export 'src/norm.dart';
 export 'src/vector.dart';
-export 'src/typed_vector.dart' show Float32x4Vector, Float64x2Vector;
+export 'src/simd_vector.dart' show Float32x4Vector, Float64x2Vector;
