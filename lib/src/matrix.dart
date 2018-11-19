34ca0d39555b9f5fdeaf7bb1a319ffcaec79ccf2
Ilya Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2018-11-19 02:12:28 +0200

    WIP

--- lib/src/matrix.dart
+++ lib/src/matrix.dart
@@ -1,4 +1,5 @@
 abstract class Matrix {
   int get rows;
   int get columns;
+  Iterable<double> operator [](int index);
 }
--- lib/src/simd/float32x4_matrix.dart
+++ lib/src/simd/float32x4_matrix.dart
@@ -1,9 +1,14 @@
+import 'dart:core';
 import 'dart:typed_data';
 
 import 'package:linalg/src/matrix.dart';
+import 'package:linalg/src/simd/matrix_iterable_mixin.dart';
 
-class Float32x4Matrix implements Matrix {
+class Float32x4Matrix extends Object with MatrixIterableMixin implements Matrix, Iterable<Iterable<double>> {
+  @override
   final int rows;
+
+  @override
   final int columns;
 
   ByteData _data;
@@ -12,9 +17,13 @@ class Float32x4Matrix implements Matrix {
     _data = ByteData(rows * columns);
     for (int i = 0; i < source.length; i++) {
       for (int j = 0; j < source[i].length; j++) {
-        _data.setFloat32(_data.lengthInBytes, source[i][j]);
+        _data.setFloat32(_data.lengthInBytes * Float32List.bytesPerElement, source[i][j]);
       }
     }
   }
 
+  @override
+  Iterable<double> operator [](int index) {
+    // TODO: implement []
+  }
 }
