36fe3e6a52196aaac9490d1a571271b3dcacabd9
Ilya Gyrdymov <ilgyrd@gmail.com>
2018-11-28 01:08:15 +0200

    submatrix refactored

--- lib/src/matrix/matrix.dart
+++ lib/src/matrix/matrix.dart
@@ -5,7 +5,7 @@ abstract class Matrix<E, T extends Vector<E>> {
   int get rowsNum;
   int get columnsNum;
   Iterable<double> operator [](int index);
-  Matrix<E, T> submatrix(Range rowsRange, Range columnsRange);
+  Matrix<E, T> submatrix({Range rows, Range columns});
   T getColumnVector(int index);
   T getRowVector(int index);
 }
