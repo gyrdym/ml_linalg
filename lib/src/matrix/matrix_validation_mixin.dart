59b7ff13a7270aa9b918cbffb2c1acec1a2669a6
Ilya Gyrdymov <ilgyrd@gmail.com>
2018-12-02 19:33:33 +0200

    validator added

---
+++ lib/src/matrix/matrix_validation_mixin.dart
@@ -0,0 +1,20 @@
+import 'package:linalg/src/matrix/matrix.dart';
+import 'package:linalg/src/vector/vector.dart';
+
+abstract class MatrixValidationMixin<E, T extends Vector<E>> {
+  void checkDimensions(Matrix<E, T> first, Matrix<E, T> second,
+      {String errorTitle = 'Cannot perform the operation'}) {
+    if (first.rowsNum != second.rowsNum || first.columnsNum != second.columnsNum) {
+      throw Exception('${errorTitle}: the matrices have different dimensions - '
+          '(${first.rowsNum} x ${first.columnsNum}) and (${second.rowsNum} x ${second.columnsNum})');
+    }
+  }
+
+  void checkColumnsAndRowsNumber(Matrix<E, T> first, Matrix<E, T> second,
+      {String errorTitle = 'Cannot perform the operation'}) {
+    if (first.columnsNum != second.rowsNum) {
+      throw Exception('$errorTitle: column number (${first.columnsNum}) of the first matrix is not equal to row '
+          'number (${second.rowsNum}) of the second matrix');
+    }
+  }
+}
