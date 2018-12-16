89e7dd5dab02c5ebd4062dfc7292885340218b78
Ilya Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2018-12-17 00:36:58 +0200

    mutable vectors supported in matrices

--- lib/src/vector/ml_vector_factory.dart
+++ lib/src/vector/ml_vector_factory.dart
@@ -2,6 +2,8 @@ import 'package:ml_linalg/vector.dart';
 import 'package:ml_linalg/vector_type.dart';
 
 abstract class MLVectorFactory<S extends List<E>, E> {
-  MLVector<E> createVectorFrom(Iterable<double> source, [MLVectorType type = MLVectorType.column]);
-  MLVector<E> createVectorFromSIMDList(S source, [int length, MLVectorType type = MLVectorType.column]);
+  MLVector<E> createVectorFrom(Iterable<double> source, [MLVectorType type = MLVectorType.column,
+    bool mutable = false]);
+  MLVector<E> createVectorFromSIMDList(S source, [int length, MLVectorType type = MLVectorType.column,
+    bool mutable = false]);
 }
