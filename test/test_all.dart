9d11bf4fc4d0791e0fa215e76cc6a5502f937218
Ilya Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2019-02-11 19:57:28 +0200

    coveralls script added

---
+++ test/test_all.dart
@@ -0,0 +1,9 @@
+import 'float32x4_matrix_test.dart' as float32x4_matrix_test;
+import 'float32_matrix_iterator_test.dart' as float32x4_matrix_iterator_test;
+import 'float32x4_vector_test.dart' as float32x4_vector_test;
+
+void main() {
+  float32x4_matrix_test.main();
+  float32x4_matrix_iterator_test.main();
+  float32x4_vector_test.main();
+}
