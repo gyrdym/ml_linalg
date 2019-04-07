5371c655047cba7e86729c5b0778fa5f729a3b6c
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-04-07 14:06:56 +0300

    WIP: update byte data function added as a parameter

--- lib/src/matrix/float32x4/float32x4_matrix.dart
+++ lib/src/matrix/float32x4/float32x4_matrix.dart
@@ -5,9 +5,12 @@ import 'package:ml_linalg/src/matrix/base_matrix.dart';
 import 'package:ml_linalg/src/matrix/data_manager/data_manager_impl.dart';
 import 'package:ml_linalg/vector.dart';
 
-final asFloat32List =
-    (ByteBuffer buffer, [int offset, int length]) =>
+final bufferToFloat32List =
+    (ByteBuffer buffer, int offset, int length) =>
         buffer.asFloat32List(offset, length);
+
+final updateByteData = (ByteData data, int offset, double value,
+    Endian endian) => data.setFloat32(offset, value, endian);
 
 class Float32x4Matrix extends BaseMatrix {
   Float32x4Matrix.from(Iterable<Iterable<double>> source) :
@@ -15,21 +18,24 @@ class Float32x4Matrix extends BaseMatrix {
           source,
           Float32List.bytesPerElement,
           Float32x4,
-          asFloat32List));
+          bufferToFloat32List,
+          updateByteData));
 
   Float32x4Matrix.columns(Iterable<Vector> source) :
         super(DataManagerImpl.fromColumns(
           source,
           Float32List.bytesPerElement,
           Float32x4,
-          asFloat32List));
+          bufferToFloat32List,
+          updateByteData));
 
   Float32x4Matrix.rows(Iterable<Vector> source) :
         super(DataManagerImpl.fromRows(
           source,
           Float32List.bytesPerElement,
           Float32x4,
-          asFloat32List));
+          bufferToFloat32List,
+          updateByteData));
 
   Float32x4Matrix.flattened(Iterable<double> source, int rowsNum,
       int columnsNum) :
@@ -39,7 +45,8 @@ class Float32x4Matrix extends BaseMatrix {
           columnsNum,
           Float32List.bytesPerElement,
           Float32x4,
-          asFloat32List));
+          bufferToFloat32List,
+          updateByteData));
 
   @override
   final Type dtype = Float32x4;
