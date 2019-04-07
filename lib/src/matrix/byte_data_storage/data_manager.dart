86f904aace142ca64a100385fac002f74c2ccea3
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-04-07 13:13:02 +0300

    WIP: matrix data manager refactored

--- lib/src/matrix/byte_data_storage/data_manager.dart
+++ lib/src/matrix/byte_data_storage/data_manager.dart
@@ -6,6 +6,9 @@ abstract class DataManager {
   Iterator<Iterable<double>> get dataIterator;
   List<Vector> get rowsCache;
   List<Vector> get columnsCache;
+  Vector getColumn(int index, {bool tryCache = true, bool mutable = false});
+  void setColumn(int columnNum, Iterable<double> columnValues);
+  Vector getRow(int index, {bool tryCache = true, bool mutable = false});
 
   void update(int idx, double value);
   void updateAll(int idx, Iterable<double> values);
