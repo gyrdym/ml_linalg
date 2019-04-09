be182a0b4747b6b00ad493d8df37ce4d56f54d1e
Ilya Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2019-04-09 23:45:04 +0300

    WIP: changelog corrected

--- test/float32x4_matrix_test.dart
+++ test/float32x4_matrix_test.dart
@@ -809,6 +809,33 @@ void main() {
       ]);
       expect(() => matrix.toVector(), throwsException);
     });
+
+    test('should return rows', () {
+      final matrix = Float32x4Matrix.from([
+        [4.0, 8.0, 12.0, 16.0],
+        [20.0, 24.0, 28.0, 32.0],
+        [36.0, .0, -8.0, -12.0],
+      ]);
+      expect(matrix.rows, equals([
+        [4.0, 8.0, 12.0, 16.0],
+        [20.0, 24.0, 28.0, 32.0],
+        [36.0, .0, -8.0, -12.0],
+      ]));
+    });
+
+    test('should return columns', () {
+      final matrix = Float32x4Matrix.from([
+        [4.0, 8.0, 12.0, 16.0],
+        [20.0, 24.0, 28.0, 32.0],
+        [36.0, .0, -8.0, -12.0],
+      ]);
+      expect(matrix.columns, equals([
+        [4.0, 20.0, 36.0],
+        [8.0, 24.0, .0],
+        [12.0, 28.0, -8.0],
+        [16.0, 32.0, -12.0],
+      ]));
+    });
   });
 
   group('Float32x4Matrix.uniqueRows', () {
