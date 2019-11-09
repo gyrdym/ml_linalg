6ef0e5fd61cd9262f3a0c7af78cdf6434a9bec91
Ilya Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2019-11-10 01:24:27 +0200

    WIP: vector filled constructor unit tests added

---
+++ test/vector/constructors/filled_constructor/vector_filled_constructor_test.dart
@@ -0,0 +1,8 @@
+import 'package:ml_linalg/dtype.dart';
+
+import 'vector_filled_constructor_test_group_factory.dart';
+
+void main() {
+  vectorFilledConstructorTestGroupFactory(DType.float32);
+  vectorFilledConstructorTestGroupFactory(DType.float64);
+}
