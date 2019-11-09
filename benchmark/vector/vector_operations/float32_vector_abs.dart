7ade051801c173cf498379cba4b6085fd1e80c88
Ilya Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2019-11-09 12:45:23 +0200

    Vector, disableCache parameter added

--- benchmark/vector/vector_operations/float32_vector_abs.dart
+++ benchmark/vector/vector_operations/float32_vector_abs.dart
@@ -5,25 +5,25 @@ import 'package:ml_linalg/src/vector/float32x4_vector.dart';
 
 const amountOfElements = 10000000;
 
-class VectorSqrtBenchmark extends BenchmarkBase {
-  VectorSqrtBenchmark()
-      : super('Vector sqrt method, $amountOfElements elements');
+class VectorAbsBenchmark extends BenchmarkBase {
+  VectorAbsBenchmark()
+      : super('Vector abs, $amountOfElements elements');
 
   Float32x4Vector vector;
 
   static void main() {
-    VectorSqrtBenchmark().report();
+    VectorAbsBenchmark().report();
   }
 
   @override
   void run() {
-    vector.sqrt();
+    vector.abs();
   }
 
   @override
   void setup() {
-    vector = Float32x4Vector.randomFilled(amountOfElements, 1, min: 0, max: 200,
-        disableCache: true);
+    vector = Float32x4Vector.randomFilled(amountOfElements, 1,
+        min: -1000, max: 2000, disableCache: true);
   }
 
   void tearDown() {
@@ -32,5 +32,5 @@ class VectorSqrtBenchmark extends BenchmarkBase {
 }
 
 void main() {
-  VectorSqrtBenchmark.main();
+  VectorAbsBenchmark.main();
 }
