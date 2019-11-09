57d59ec6c166d8f0ea82f433b963329edaf895e3
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-11-10 01:32:16 +0200

    WIP: vector random filled constructor unit tests added

---
+++ test/vector/constructors/random_filled_constructor/vector_random_filled_constructor_test.dart
@@ -0,0 +1,8 @@
+import 'package:ml_linalg/dtype.dart';
+
+import 'vector_random_filled_constructor_test_group_factory.dart';
+
+void main() {
+  vectorRandomFilledConstructorTestGroupFactory(DType.float32);
+  vectorRandomFilledConstructorTestGroupFactory(DType.float64);
+}
