38855acb7d3ffa44b76e828f61388a25347a0599
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-11-11 00:20:10 +0200

    Vector: a better hash code provided

--- lib/src/vector/float64x2_vector.gen.dart
+++ lib/src/vector/float64x2_vector.gen.dart
@@ -103,8 +103,6 @@ class Float64x2Vector with IterableMixin<double> implements Vector {
 
   bool get _isLastBucketNotFull => length % _bucketSize > 0;
 
-  int _hash;
-
   @override
   bool operator ==(Object other) {
     if (other is Float64x2Vector) {
@@ -123,15 +121,18 @@ class Float64x2Vector with IterableMixin<double> implements Vector {
   }
 
   @override
-  int get hashCode => _cacheManager.retrieve('hash',
-          () => length > 0 ? _generateHash() : 0);
+  int get hashCode => _cacheManager.retrieve('hash', () {
+    if (isEmpty) {
+      return 0;
+    }
 
-  int _generateHash() {
     int i = 0;
-    final simdHash = _innerSimdList.reduce(
+
+    final summed = _innerSimdList.reduce(
             (sum, element) => sum + element.scale((31 * (i++)) * 1.0));
-    return _simdHelper.sumLanesForHash(simdHash) ~/ 1;
-  }
+
+    return length + _simdHelper.sumLanesForHash(summed).hashCode;
+  }, skipCaching: false);
 
   @override
   Vector operator +(Object value) {
