97e2ddb010c7b40a932658212a09b356bfc3f8a5
Ilya Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2019-11-10 12:53:25 +0200

    WIP: vector min method unit tests added

--- test/vector/methods/min/min_test_group_factory.dart
+++ test/vector/methods/min/min_test_group_factory.dart
@@ -7,29 +7,26 @@ import '../../../dtype_to_class_name_mapping.dart';
 void vectorMinTestGroupFactory(DType dtype) =>
     group(dtypeToVectorClassName[dtype], () {
       group('min method', () {
-        test('should find a minimal element for the vector with more than 4 '
+        test('should find the minimal element for the vector with more than 4 '
             'elements', () {
           final vector = Vector.fromList([10.0, 1.0, 4.0, 7.0, 9.0, 1.0],
               dtype: dtype);
 
           expect(vector.min(), 1.0);
-          expect(vector.dtype, dtype);
         });
 
-        test('should find a minimal element for the vector with 4 '
+        test('should find the minimal element for the vector with 4 '
             'elements', () {
           final vector = Vector.fromList([10.0, 0.0, 4.0, 7.0], dtype: dtype);
 
           expect(vector.min(), 0.0);
-          expect(vector.dtype, dtype);
         });
 
-        test('should find a minimal element for the vector with length that is '
-            'less than 4', () {
+        test('should find the minimal element for the vector with length that '
+            'is less than 4', () {
           final vector = Vector.fromList([10.0, 1.0, 4.0], dtype: dtype);
 
           expect(vector.min(), 1.0);
-          expect(vector.dtype, dtype);
         });
       });
     });
