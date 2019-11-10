6ae0db8938fb8f3189e3028378724b4e2c79f6c9
Ilya Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2019-11-10 18:34:41 +0200

    WIP: vector sample method unit tests added

--- test/float32_vector_test.dart
+++ test/float32_vector_test.dart
@@ -1,17 +1,8 @@
 import 'package:ml_linalg/linalg.dart';
-import 'package:ml_tech/unit_testing/matchers/iterable_almost_equal_to.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Float32Vector', () {
-    test('should create a vector using elements on specific inidces from '
-        'given list', () {
-      final vector = Vector.fromList([10.0, 3.0, 4.0, 7.0, 9.0, 12.0]);
-      final query = vector.sample([1, 1, 0, 3]);
-      expect(query, equals([3.0, 3.0, 10.0, 7.0]));
-      expect(() => vector.sample([20, 0, 1]), throwsRangeError);
-    });
-
     test('`unique` method', () {
       final vector = Vector.fromList([
         10.0,
