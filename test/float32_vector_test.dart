c4c697b9365efcd738ffa0140db03b71f8fb1c1b
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-11-10 12:57:46 +0200

    WIP: vector sqrt method unit tests added

--- test/float32_vector_test.dart
+++ test/float32_vector_test.dart
@@ -2,18 +2,17 @@ import 'dart:typed_data';
 
 import 'package:ml_linalg/distance.dart';
 import 'package:ml_linalg/linalg.dart';
-import 'package:ml_linalg/src/vector/float32x4_vector.dart';
 import 'package:ml_tech/unit_testing/matchers/iterable_almost_equal_to.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Float32Vector', () {
-    Float32x4Vector vector1;
-    Float32x4Vector vector2;
+    Vector vector1;
+    Vector vector2;
 
     setUp(() {
-      vector1 = Float32x4Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
-      vector2 = Float32x4Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
+      vector1 = Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
+      vector2 = Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
     });
 
     tearDown(() {
@@ -23,7 +22,7 @@ void main() {
 
     test('should map an existing vector to a new one processing 4 elements in '
         'a time', () {
-      final vector = Float32x4Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
+      final vector = Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
       int iteration = 0;
       final actual = vector.fastMap((Float32x4 element) {
         iteration++;
@@ -68,7 +67,7 @@ void main() {
     });
 
     test('should perform substruction of a scalar', () {
-      final vector = Float32x4Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
+      final vector = Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
       final result = vector - 13.0;
       expect(result != vector, isTrue);
       expect(result.length, equals(5));
@@ -76,63 +75,63 @@ void main() {
     });
 
     test('should find Euclidean distance (from vector to the same vector)', () {
-      final vector = Float32x4Vector.fromList([1.0, 2.0, 3.0, 4.0]);
+      final vector = Vector.fromList([1.0, 2.0, 3.0, 4.0]);
       final distance = vector.distanceTo(vector);
       expect(distance, equals(0.0),
           reason: 'Wrong vector distance calculation');
     });
 
     test('should find euclidean distance', () {
-      final vector1 = Float32x4Vector.fromList([10.0, 3.0, 4.0, 7.0, 9.0, 12.0]);
-      final vector2 = Float32x4Vector.fromList([1.0, 3.0, 2.0, 11.5, 10.0, 15.5]);
+      final vector1 = Vector.fromList([10.0, 3.0, 4.0, 7.0, 9.0, 12.0]);
+      final vector2 = Vector.fromList([1.0, 3.0, 2.0, 11.5, 10.0, 15.5]);
       expect(vector1.distanceTo(vector2, distance: Distance.euclidean),
           equals(10.88577052853862));
     });
 
     test('should find manhattan distance', () {
-      final vector1 = Float32x4Vector.fromList([10.0, 3.0, 4.0, 7.0, 9.0, 12.0]);
-      final vector2 = Float32x4Vector.fromList([1.0, 3.0, 2.0, 11.5, 10.0, 15.5]);
+      final vector1 = Vector.fromList([10.0, 3.0, 4.0, 7.0, 9.0, 12.0]);
+      final vector2 = Vector.fromList([1.0, 3.0, 2.0, 11.5, 10.0, 15.5]);
       expect(vector1.distanceTo(vector2, distance: Distance.manhattan),
           equals(20.0));
     });
 
     test('should find cosine distance (the same vectors)', () {
-      final vector1 = Float32x4Vector.fromList([1.0, 0.0]);
-      final vector2 = Float32x4Vector.fromList([1.0, 0.0]);
+      final vector1 = Vector.fromList([1.0, 0.0]);
+      final vector2 = Vector.fromList([1.0, 0.0]);
       expect(vector1.distanceTo(vector2, distance: Distance.cosine),
           equals(0.0));
     });
 
     test('should find cosine distance (different vectors)', () {
-      final vector1 = Float32x4Vector.fromList([4.0, 3.0]);
-      final vector2 = Float32x4Vector.fromList([2.0, 4.0]);
+      final vector1 = Vector.fromList([4.0, 3.0]);
+      final vector2 = Vector.fromList([2.0, 4.0]);
       expect(vector1.distanceTo(vector2, distance: Distance.cosine),
           closeTo(0.1055, 1e-4));
     });
 
     test('should find cosine distance (different vectors with negative '
         'elements)', () {
-      final vector1 = Float32x4Vector.fromList([4.0, -3.0]);
-      final vector2 = Float32x4Vector.fromList([-2.0, 4.0]);
+      final vector1 = Vector.fromList([4.0, -3.0]);
+      final vector2 = Vector.fromList([-2.0, 4.0]);
       expect(vector1.distanceTo(vector2, distance: Distance.cosine),
           closeTo(1.8944, 1e-4));
     });
 
     test('should find cosine distance (one of two vectors is zero-vector)', () {
-      final vector1 = Float32x4Vector.fromList([0.0, 0.0]);
-      final vector2 = Float32x4Vector.fromList([-2.0, 4.0]);
+      final vector1 = Vector.fromList([0.0, 0.0]);
+      final vector2 = Vector.fromList([-2.0, 4.0]);
       expect(() => vector1.distanceTo(vector2, distance: Distance.cosine),
           throwsException);
     });
 
     test('should find vector norm', () {
-      final vector = Float32x4Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
+      final vector = Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
       expect(vector.norm(Norm.euclidean), equals(closeTo(7.41, 1e-2)));
       expect(vector.norm(Norm.manhattan), equals(15.0));
     });
 
     test('should normalize itself (eucleadean norm)', () {
-      final vector = Float32x4Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
+      final vector = Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
       final actual = vector.normalize(Norm.euclidean);
       final expected = [0.134, 0.269, 0.404, 0.539, 0.674];
       expect(actual, iterableAlmostEqualTo(expected, 1e-3));
@@ -140,7 +139,7 @@ void main() {
     });
 
     test('should normalize itself (Manhattan norm)', () {
-      final vector = Float32x4Vector.fromList([1.0, -2.0, 3.0, -4.0, 5.0]);
+      final vector = Vector.fromList([1.0, -2.0, 3.0, -4.0, 5.0]);
       final actual = vector.normalize(Norm.manhattan);
       final expected = [1 / 15, -2 / 15, 3 / 15, -4 / 15, 5 / 15];
       expect(actual, iterableAlmostEqualTo(expected, 1e-3));
@@ -148,7 +147,7 @@ void main() {
     });
 
     test('should rescale its every element into range [0...1]', () {
-      final vector = Float32x4Vector.fromList([1.0, -2.0, 3.0, -4.0, 5.0, 0.0]);
+      final vector = Vector.fromList([1.0, -2.0, 3.0, -4.0, 5.0, 0.0]);
       final actual = vector.rescale(); // min = -4, diff = 9
       final expected = [5 / 9, 2 / 9, 7 / 9, 0.0, 1.0, 4 / 9];
       expect(actual, iterableAlmostEqualTo(expected, 1e-3));
@@ -156,12 +155,12 @@ void main() {
     });
 
     test('should find vector elements sum', () {
-      final vector = Float32x4Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
+      final vector = Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0]);
       expect(vector.sum(), equals(15.0));
     });
 
     test('should find vector elements absolute value', () {
-      final vector = Float32x4Vector.fromList([-3.0, 4.5, -12.0, -23.5, 44.0]);
+      final vector = Vector.fromList([-3.0, 4.5, -12.0, -23.5, 44.0]);
       final result = vector.abs();
       expect(result, equals([3.0, 4.5, 12.0, 23.5, 44.0]));
       expect(result, isNot(vector));
@@ -169,14 +168,14 @@ void main() {
 
     test('should create a vector using elements on specific inidces from '
         'given list', () {
-      final vector = Float32x4Vector.fromList([10.0, 3.0, 4.0, 7.0, 9.0, 12.0]);
+      final vector = Vector.fromList([10.0, 3.0, 4.0, 7.0, 9.0, 12.0]);
       final query = vector.sample([1, 1, 0, 3]);
       expect(query, equals([3.0, 3.0, 10.0, 7.0]));
       expect(() => vector.sample([20, 0, 1]), throwsRangeError);
     });
 
     test('`unique` method', () {
-      final vector = Float32x4Vector.fromList([
+      final vector = Vector.fromList([
         10.0,
         3.0,
         4.0,
@@ -194,18 +193,6 @@ void main() {
       ]);
       final unique = vector.unique();
       expect(unique, equals([10.0, 3.0, 4.0, 0.0, 7.0, 12.0, 9.0]));
-    });
-
-    group('sqrt', () {
-      test('should extract square root of each element', () {
-        final vector = Float32x4Vector.fromList([4, 25, 9]);
-        expect(vector.sqrt(), equals([2, 5, 3]));
-      });
-
-      test('should return NaN value for negative elements', () {
-        final vector = Float32x4Vector.fromList([-4, -25, -9]);
-        expect(vector.sqrt(), equals([isNaN, isNaN, isNaN]));
-      });
     });
   });
 }
---
+++ test/vector/methods/sqrt/sqrt_test_group_factory.dart
@@ -0,0 +1,22 @@
+import 'package:ml_linalg/dtype.dart';
+import 'package:ml_linalg/vector.dart';
+import 'package:test/test.dart';
+
+import '../../../dtype_to_class_name_mapping.dart';
+
+void vectorSqrtTestGroupFactory(DType dtype) =>
+    group(dtypeToVectorClassName[dtype], () {
+      group('sqrt method', () {
+        test('should extract square root of each element', () {
+          final vector = Vector.fromList([4, 25, 9], dtype: dtype);
+
+          expect(vector.sqrt(), equals([2, 5, 3]));
+        });
+
+        test('should return NaN value for negative elements', () {
+          final vector = Vector.fromList([-4, -25, -9], dtype: dtype);
+
+          expect(vector.sqrt(), equals([isNaN, isNaN, isNaN]));
+        });
+      });
+    });
