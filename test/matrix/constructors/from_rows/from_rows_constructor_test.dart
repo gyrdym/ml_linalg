e9f1f88bebdb0f34570f85aabb3e5c04731c6b00
Ilya Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2019-11-15 00:54:20 +0200

    Matrix constructors unit tests extracted to separate directories

---
+++ test/matrix/constructors/from_rows/from_rows_constructor_test.dart
@@ -0,0 +1,8 @@
+import 'package:ml_linalg/dtype.dart';
+
+import 'from_rows_constructor_test_group_factory.dart';
+
+void main() {
+  matrixFromRowsConstructorTestGroupFactory(DType.float32);
+  matrixFromRowsConstructorTestGroupFactory(DType.float64);
+}
