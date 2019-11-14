ab8113249e6c3cbaa212a80f9d07a28ff2bad75b
Ilya Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2019-11-15 01:40:59 +0200

    Matrix: reduceRows and reduceColumns tests extracted to separate directories

--- test/float32_matrix_test.dart
+++ test/float32_matrix_test.dart
@@ -9,57 +9,6 @@ import 'package:test/test.dart';
 
 void main() {
   group('Matrix', () {
-    test('should reduce all the matrix rows into a single vector, without '
-        'initial reducer value', () {
-      final matrix = Matrix.fromList([
-        [11.0, 12.0, 13.0, 14.0],
-        [15.0, 16.0, 17.0, 18.0],
-        [21.0, 22.0, 23.0, 24.0],
-      ]);
-      final actual = matrix.reduceRows((combine, vector) => combine + vector);
-      final expected = [47, 50, 53, 56];
-      expect(actual, equals(expected));
-    });
-
-    test('should reduce all the matrix rows into a single vector, with initial '
-        'reducer value', () {
-      final matrix = Matrix.fromList([
-        [11.0, 12.0, 13.0, 14.0],
-        [15.0, 16.0, 17.0, 18.0],
-        [21.0, 22.0, 23.0, 24.0],
-      ]);
-      final actual = matrix.reduceRows((combine, vector) => combine + vector,
-          initValue: Vector.fromList([2.0, 3.0, 4.0, 5.0]));
-      final expected = [49, 53, 57, 61];
-      expect(actual, equals(expected));
-    });
-
-    test('should reduce all the matrix columns into a single vector, without '
-        'initial reducer value', () {
-      final matrix = Matrix.fromList([
-        [11.0, 12.0, 13.0, 14.0],
-        [15.0, 16.0, 17.0, 18.0],
-        [21.0, 22.0, 23.0, 24.0],
-      ]);
-      final actual =
-          matrix.reduceColumns((combine, vector) => combine + vector);
-      final expected = [50, 66, 90];
-      expect(actual, equals(expected));
-    });
-
-    test('should reduce all the matrix columns into a single vector, with '
-        'initial reducer value', () {
-      final matrix = Matrix.fromList([
-        [11.0, 12.0, 13.0, 14.0],
-        [15.0, 16.0, 17.0, 18.0],
-        [21.0, 22.0, 23.0, 24.0],
-      ]);
-      final actual = matrix.reduceColumns((combine, vector) => combine + vector,
-          initValue: Vector.fromList([2.0, 3.0, 4.0]));
-      final expected = [52, 69, 94];
-      expect(actual, equals(expected));
-    });
-
     test('should perform column-wise mapping of the matrix to a new one', () {
       final matrix = Matrix.fromList([
         [11.0, 12.0, 13.0, 14.0],
