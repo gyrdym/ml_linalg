4d1b4886b68a311462b5b3ce76e5368e90d86647
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-11-16 20:31:11 +0200

    Matrix: unit tests extracted into separate directories

---
+++ test/matrix/methods/sample/sample_test.dart
@@ -0,0 +1,8 @@
+import 'package:ml_linalg/dtype.dart';
+
+import 'sample_test_group_factory.dart';
+
+void main() {
+  matrixSampleTestGroupFactory(DType.float32);
+  matrixSampleTestGroupFactory(DType.float64);
+}
