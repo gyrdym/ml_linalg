477cd64aed768f1a4d3cf52e6b0198ce03f1f793
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-11-23 19:34:47 +0200

    Matrix fromList constructor fixed

--- lib/src/matrix/data_manager/float32_matrix_data_manager.dart
+++ lib/src/matrix/data_manager/float32_matrix_data_manager.dart
@@ -22,6 +22,10 @@ class Float32MatrixDataManager implements MatrixDataManager {
             getLengthOfFirstOrZero(source) * _bytesPerElement) {
     final dataAsList = _data.buffer.asFloat32List();
     for (int i = 0; i < source.length; i++) {
+      if (source[i].length != columnsNum) {
+        throw Exception('Wrong nested list length: ${source[i].length}, '
+            'expected length: ${columnsNum}');
+      }
       for (int j = 0; j < source[i].length; j++) {
         dataAsList[i * columnsNum + j] = source[i][j];
       }
