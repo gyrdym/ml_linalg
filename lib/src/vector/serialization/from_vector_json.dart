87d42e53af276007a6219ffb573e1d29803b1680
Ilya Gyrdymov <ilgyrd@gmail.com>
2020-05-21 00:26:17 +0300

    unit tests for fromVectorJson added

--- lib/src/vector/serialization/from_vector_json.dart
+++ lib/src/vector/serialization/from_vector_json.dart
@@ -3,14 +3,16 @@ import 'package:ml_linalg/src/common/dtype_serializer/dtype_encoded_values.dart'
 import 'package:ml_linalg/src/vector/vector_json_keys.dart';
 
 Vector fromVectorJson(Map<String, dynamic> json) {
-  final source = json[vectorDataJsonKey] as List<double>;
+  final source = (json[vectorDataJsonKey] as List)
+      .map((dynamic value) => double.parse(value.toString()))
+      .toList(growable: false);
 
   switch(json[vectorDTypeJsonKey] as String) {
     case dTypeFloat32EncodedValue:
       return Vector.fromList(source, dtype: DType.float32);
 
     case dTypeFloat64EncodedValue:
-      return Vector.fromList(source, dtype: DType.float32);
+      return Vector.fromList(source, dtype: DType.float64);
 
     default:
       throw UnsupportedError('Unknown dtype encoded value - '
