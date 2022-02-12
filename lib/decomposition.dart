24ead8576ad380e55bfc5052ccf7d8e2ac0879d4
Ilya Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2022-02-12 17:18:27 +0200

    Added documentation for matrix decomposition and inversion (#118)

--- lib/decomposition.dart
+++ lib/decomposition.dart
@@ -1,4 +1,7 @@
 enum Decomposition {
+  /// Cholesky decomposition. Applicable only for for positive definite and symmetric matrices
   cholesky,
+
+  /// LU (Lower-Upper) decomposition
   LU,
 }
--- lib/inverse.dart
+++ lib/inverse.dart
@@ -1,6 +1,15 @@
 enum Inverse {
+  /// Matrix inversion that is based on Cholesky decomposition which means that
+  /// only positive definite and symmetric matrices can be inverted using this kind
+  /// of inversion
   cholesky,
+
+  /// Matrix inversion which is based on LU decomposition
   LU,
+
+  /// This kind of matrix inversion is applicable only for lower-triangular matrices
   forwardSubstitution,
+
+  /// This kind of matrix inversion is applicable only for upper-triangular matrices
   backwardSubstitution,
 }
