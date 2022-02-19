46afcbc53638a4c5be958eaac318e2fbaf58e2ef
Ilya Gyrdymov <ilgyrd@gmail.com>
2022-02-20 01:40:54 +0200

    Matrix: Added 'filterColumns' method

---
+++ test/integration_test/vector/methods/filter_elements/filter_elements_test.dart
@@ -0,0 +1,8 @@
+import 'package:ml_linalg/dtype.dart';
+
+import 'filter_elements_test_group_factory.dart';
+
+void main() {
+  filterElementsTestGroupFactory(DType.float32);
+  filterElementsTestGroupFactory(DType.float64);
+}
