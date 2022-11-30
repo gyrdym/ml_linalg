86cd2390a6bb378c1c8ff3afd94adf71d0a90e26
Ilia Gyrdymov <ilgyrd@gmail.com>
2022-11-30 23:51:27 +0200

    Vector arithmetic operators: input types narrowed (#129)

--- lib/src/common/exception/unsupported_operand_type_exception.dart
+++ lib/src/common/exception/unsupported_operand_type_exception.dart
@@ -1,6 +1,7 @@
 class UnsupportedOperandTypeException implements Exception {
-  UnsupportedOperandTypeException(Type operandType)
-      : _message = 'Unsupported operand type: $operandType';
+  UnsupportedOperandTypeException(Type operandType, {String operationName = ''})
+      : _message =
+            '${operationName.isNotEmpty ? '$operationName, ' : ''}Unsupported operand type: $operandType';
 
   final String _message;
 
