70332c616e96e4236e94ca07a48a05f4b6178a61
Ilia Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2022-12-16 23:57:53 +0200

    Cholesky decomposition speed up (#137)

--- benchmark/matrix/float32/matrix_operations/float32_inverse_cholesky.dart
+++ benchmark/matrix/float32/matrix_operations/float32_inverse_cholesky.dart
@@ -1,3 +1,5 @@
+// Approx. 7 second (MacBook Pro 2019), Dart version: 2.16.0
+
 import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:ml_linalg/dtype.dart';
 import 'package:ml_linalg/inverse.dart';
@@ -7,7 +9,7 @@ class Float32MatrixInverseCholeskyBenchmark extends BenchmarkBase {
   Float32MatrixInverseCholeskyBenchmark()
       : super('Matrix float32 inverse method (Cholesky)');
 
-  final Matrix _source = Matrix.randomSPD(300, dtype: DType.float32);
+  final Matrix source = Matrix.randomSPD(300, dtype: DType.float32, seed: 12);
 
   static void main() {
     Float32MatrixInverseCholeskyBenchmark().report();
@@ -15,7 +17,7 @@ class Float32MatrixInverseCholeskyBenchmark extends BenchmarkBase {
 
   @override
   void run() {
-    _source.inverse(Inverse.cholesky);
+    source.inverse(Inverse.cholesky);
   }
 }
 
