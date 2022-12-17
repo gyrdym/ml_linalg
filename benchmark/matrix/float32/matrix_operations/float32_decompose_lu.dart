9f37ea8f83785284a3839841fe0c923291fece0a
Ilya Gyrdymov <ilgyrd@gmail.com>
2022-12-17 12:44:28 +0200

    LU decomposition speed up

--- benchmark/matrix/float32/matrix_operations/float32_decompose_lu.dart
+++ benchmark/matrix/float32/matrix_operations/float32_decompose_lu.dart
@@ -1,4 +1,4 @@
-// Approx. 12.5 seconds (MacBook Pro 2019), Dart version: 2.16.0
+// Approx. 11 seconds (MacBook Pro 2019), Dart version: 2.16.0
 
 import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:ml_linalg/decomposition.dart';
