c0294c378af9f2fe6fbe7831ae2b18bb55607a53
Ilia Gyrdymov <ilgyrd@gmail.com>
2023-01-03 16:15:43 +0200

    Matrix: 'pow', 'sum', 'log', 'exp', 'prod' methods speed up (#156)

--- test/matrix/methods/log/log_test_group_factory.dart
+++ test/matrix/methods/log/log_test_group_factory.dart
@@ -47,6 +47,74 @@ void matrixLogTestGroupFactory(DType dtype) =>
           expect(result.dtype, dtype);
         });
 
+        test(
+            'should take a natural log of every matrix element and return '
+            'new matrix, 9x1 matrix, fromFlattenedList constructor', () {
+          final matrix = Matrix.fromFlattenedList([
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+          ], 9, 1, dtype: dtype);
+          final result = matrix.log();
+
+          expect(
+              result,
+              iterable2dAlmostEqualTo([
+                [math.log(1)],
+                [math.log(2)],
+                [math.log(3)],
+                [math.log(4)],
+                [math.log(5)],
+                [math.log(6)],
+                [math.log(7)],
+                [math.log(8)],
+                [math.log(9)],
+              ], 1e-3));
+          expect(result.dtype, dtype);
+          expect(result, isNot(same(matrix)));
+        });
+
+        test(
+            'should take a natural log of every matrix element and return '
+            'new matrix, 1x9 matrix, fromFlattenedList constructor', () {
+          final matrix = Matrix.fromFlattenedList([
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+          ], 1, 9, dtype: dtype);
+          final result = matrix.log();
+
+          expect(
+              result,
+              iterable2dAlmostEqualTo([
+                [
+                  math.log(1),
+                  math.log(2),
+                  math.log(3),
+                  math.log(4),
+                  math.log(5),
+                  math.log(6),
+                  math.log(7),
+                  math.log(8),
+                  math.log(9)
+                ],
+              ], 1e-3));
+          expect(result.dtype, dtype);
+          expect(result, isNot(same(matrix)));
+        });
+
         test('should handle empty matrix', () {
           final matrix = Matrix.fromList([], dtype: dtype);
           final result = matrix.log();
