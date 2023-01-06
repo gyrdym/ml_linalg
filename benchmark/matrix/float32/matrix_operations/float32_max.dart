50f6a18f81d8bc9b88cfc655c1385f4789b2dea8
Ilia Gyrdymov <2048325+gyrdym@users.noreply.github.com>
2023-01-06 13:16:21 +0200

    Matrix: 'max' and 'min' methods speed up (#158)

---
+++ benchmark/matrix/float32/matrix_operations/float32_max.dart
@@ -0,0 +1,28 @@
+// Approx. 2.1 seconds (MacBook Pro 2019), Dart version: 2.16.0
+
+import 'package:benchmark_harness/benchmark_harness.dart';
+import 'package:ml_linalg/dtype.dart';
+import 'package:ml_linalg/matrix.dart';
+
+const numOfRows = 20001;
+const numOfColumns = 20001;
+
+class Float32MatrixMaxBenchmark extends BenchmarkBase {
+  Float32MatrixMaxBenchmark() : super('Matrix float32 max method');
+
+  final Matrix _source =
+      Matrix.random(numOfRows, numOfColumns, dtype: DType.float32, seed: 12);
+
+  static void main() {
+    Float32MatrixMaxBenchmark().report();
+  }
+
+  @override
+  void run() {
+    _source.max();
+  }
+}
+
+void main() {
+  Float32MatrixMaxBenchmark.main();
+}
--- test/matrix/methods/max/max_test_group_factory.dart
+++ test/matrix/methods/max/max_test_group_factory.dart
@@ -19,5 +19,108 @@ void matrixMaxTestGroupFactory(DType dtype) =>
 
           expect(actual, equals(expected));
         });
+
+        test('should find the matrix max value, 2x5 matrix', () {
+          final matrix = Matrix.fromList([
+            [1.0, 2.0, 3.0, 4.0, 7.0],
+            [5.0, 6.0, 7.0, 8.0, 13.5],
+          ], dtype: dtype);
+
+          final actual = matrix.max();
+          final expected = 13.5;
+
+          expect(actual, equals(expected));
+        });
+
+        test('should find the matrix max value, 1x5 matrix', () {
+          final matrix = Matrix.fromList([
+            [1.0, 2.0, 3.0, 4.0, 7.0],
+          ], dtype: dtype);
+
+          final actual = matrix.max();
+          final expected = 7;
+
+          expect(actual, equals(expected));
+        });
+
+        test('should find the matrix max value, 5x1 matrix', () {
+          final matrix = Matrix.fromList([
+            [1.0],
+            [2.0],
+            [3.0],
+            [4.0],
+            [7.0],
+          ], dtype: dtype);
+
+          final actual = matrix.max();
+          final expected = 7;
+
+          expect(actual, equals(expected));
+        });
+
+        test(
+            'should find the matrix max value, 1x5 matrix, fromFlattenedList constructor',
+            () {
+          final matrix = Matrix.fromFlattenedList([
+            1.0,
+            2.0,
+            3.0,
+            4.0,
+            7.0,
+          ], 1, 5, dtype: dtype);
+
+          final actual = matrix.max();
+          final expected = 7;
+
+          expect(actual, equals(expected));
+        });
+
+        test(
+            'should find the matrix max value, 1x9 matrix, fromFlattenedList constructor',
+            () {
+          final matrix = Matrix.fromFlattenedList(
+              [1, 2, 3, 4, 2344.9999, -2, -1000, -10000, 2345, 7, 10], 1, 9,
+              dtype: dtype);
+
+          final actual = matrix.max();
+          final expected = 2345;
+
+          expect(actual, equals(expected));
+        });
+
+        test(
+            'should find the matrix max value, 1x1 matrix, fromFlattenedList constructor',
+            () {
+          final matrix = Matrix.fromFlattenedList([1], 1, 1, dtype: dtype);
+
+          final actual = matrix.max();
+          final expected = 1;
+
+          expect(actual, equals(expected));
+        });
+
+        test(
+            'should find the matrix max value, 1x2 matrix, fromFlattenedList constructor, the matrix has infinite value',
+            () {
+          final matrix = Matrix.fromFlattenedList([double.infinity, 1e10], 1, 2,
+              dtype: dtype);
+
+          final actual = matrix.max();
+          final expected = double.infinity;
+
+          expect(actual, equals(expected));
+        });
+
+        test(
+            'should find the matrix max value, 1x2 matrix, fromFlattenedList constructor, the matrix has negative values',
+            () {
+          final matrix =
+              Matrix.fromFlattenedList([-100, -1e10], 1, 2, dtype: dtype);
+
+          final actual = matrix.max();
+          final expected = -100;
+
+          expect(actual, equals(expected));
+        });
       });
     });
