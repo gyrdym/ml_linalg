5f89daa1611915e6920b5c2be3331bdd05100f53
Ilya Gyrdymov <ilgyrd@gmail.com>
2023-01-08 14:05:00 +0200

    Matrix: 'mean' method speed up

--- benchmark/matrix/float32/matrix_operations/float32_mean_column.dart
+++ benchmark/matrix/float32/matrix_operations/float32_mean_column.dart
@@ -1,19 +1,19 @@
-// Approx. 2.4 seconds (MacBook Pro 2019), Dart version: 2.16.0
+// Approx. 3.0 seconds (MacBook Pro 2019), Dart version: 2.16.0
 
 import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:ml_linalg/axis.dart';
 import 'package:ml_linalg/dtype.dart';
 import 'package:ml_linalg/matrix.dart';
 
-const numOfRows = 20000;
-const numOfColumns = 2000;
+const rowCount = 20001;
+const columnCount = 2001;
 
 class Float32MatrixMeanBenchmark extends BenchmarkBase {
   Float32MatrixMeanBenchmark()
       : super('Matrix float32, matrix mean column-wise');
 
   final Matrix _source =
-      Matrix.random(numOfRows, numOfColumns, dtype: DType.float32, seed: 12);
+      Matrix.random(rowCount, columnCount, dtype: DType.float32, seed: 12);
 
   static void main() {
     Float32MatrixMeanBenchmark().report();
