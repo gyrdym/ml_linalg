0e48ce647930632259bcd2253ea47115f34856d1
Ilya Gyrdymov <ilgyrd@gmail.com>
2023-12-24 18:01:38 +0200

    Matrix: add 'solve' method

--- test/matrix/methods/solve/solve_test_group_factory.dart
+++ test/matrix/methods/solve/solve_test_group_factory.dart
@@ -8,7 +8,25 @@ import '../../../helpers.dart';
 void matrixSolveTestGroupFactory(DType dtype) =>
     group(dtypeToMatrixTestTitle[dtype], () {
       group('solve', () {
-        test('should solve system of linear equations', () {
+        test('should solve system of linear equations, 2x2 matrix', () {
+          final A = Matrix.fromList([
+            [3, 1],
+            [4, 3],
+          ], dtype: dtype);
+          final B = Matrix.fromList([
+            [3],
+            [17],
+          ], dtype: dtype);
+          final actual = A.solve(B);
+          final expected = [
+            [-1.6],
+            [7.8],
+          ];
+
+          expect(actual, iterable2dAlmostEqualTo(expected, 1e-6));
+        });
+
+        test('should solve system of linear equations, 3x3 matrix', () {
           final A = Matrix.fromList([
             [1, 1, 1],
             [0, 2, 5],
