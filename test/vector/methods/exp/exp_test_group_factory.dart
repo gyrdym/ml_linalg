d80138805dc2edc8be329927d26aa4c99b2ef8ae
Ilya Gyrdymov <ilgyrd@gmail.com>
2023-12-26 13:17:56 +0200

    Vector: extracting values from cache for 'exp' and 'prod' methods fixed

--- test/vector/methods/exp/exp_test_group_factory.dart
+++ test/vector/methods/exp/exp_test_group_factory.dart
@@ -18,5 +18,17 @@ void vectorExpTestGroupFactory(DType dtype) =>
               iterableAlmostEqualTo(
                   [2.7182, 7.3890, 20.08553, 54.5981, 148.4131], 1e-3));
         });
+
+        test('should extract value from cache', () {
+          final vector =
+              Vector.fromList([1.0, 2.0, 3.0, 4.0, 5.0], dtype: dtype);
+
+          final log = vector.log();
+          final exp = vector.exp();
+          final exp2 = vector.exp();
+
+          expect(exp, exp2);
+          expect(exp, isNot(log));
+        });
       });
     });
